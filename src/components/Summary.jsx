db4ecc0ce257f3e4f375380fb014151f1f534bbd
eddex <eddex@outlook.com>
2022-12-10 11:05:29 +0100

    feat: aggregated total balance of all wallets

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,5 +1,22 @@
 const Summary = (props) => {
-  return <h1>Summary</h1>;
+  const totalAda = () => {
+    if (!props.wallets) return 0;
+    let total = 0;
+    console.log(props.wallets);
+    props.wallets.forEach((wallet, i) => {
+      console.log(wallet);
+      total += wallet.total_balance / 1000000;
+    });
+    return total;
+  };
+  return (
+    <div className="mt-3">
+      <h1>Summary</h1>
+      <p>
+        Total balance: <b>{totalAda()} ADA</b>
+      </p>
+    </div>
+  );
 };
 
 export default Summary;
